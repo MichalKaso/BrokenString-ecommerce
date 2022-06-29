f21e304e1f4aaa7daad5822afc06b68219e4501e
Mike <michalkaso@yahoo.com>
2022-06-29 12:20:33 +0300

    final commit BrokenStrings LTD

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 Broken String LTD All rights reserved</p>
+      <p> Crafted and Designed by: <a href="https://github.com/MichalKaso" className='mike'>Michal Kaso</a></p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
